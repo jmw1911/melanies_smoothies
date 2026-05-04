1f59d0f416b0474f820dcb1962d83585228f1576
jmw1911 <jwynkoop@hotmail.com>
2026-05-03 21:55:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 ##st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+
             ##st.stop()
 
 
@@ -65,7 +65,7 @@ if ingredients_list:
             ##st.dataframe(data=my_dataframe, use_container_width=True)
             #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
             ##st.stop()
-
+      st.dataframe(pd_df)
             
   
   ##  my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
