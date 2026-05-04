4693cb35a26225935a27aea92b5a594b2435cc1b
jmw1911 <jwynkoop@hotmail.com>
2026-05-03 21:48:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ##st.dataframe(data=my_dataframe, use_container_width=True)
-
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+            ##st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -43,9 +45,7 @@ if ingredients_list:
     else:
         
         ingredients_string = ''
-        pd_df=my_dataframe.to_pandas()
-        st.dataframe(pd_df)
-            ##st.stop()
+        
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
 
