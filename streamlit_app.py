ef8a689249ead6a5cd4055b4e6052d52c6624397
jmw1911 <jwynkoop@hotmail.com>
2026-05-03 21:00:38 -0400

    Update streamlit_app.py
    json data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,12 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
+  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+  
    ## st.write(my_insert_stmt)
     
     time_to_insert = st.button('Sumbit Order')
