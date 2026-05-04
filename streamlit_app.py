51ed3e33e077af8371095ac9da730727b1cbeb2c
jmw1911 <jwynkoop@hotmail.com>
2026-05-03 20:54:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
+
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
 st.write(
