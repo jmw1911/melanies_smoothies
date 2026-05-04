05d373a45c2989f1e56ef501cdd6afbed74e2088
jmw1911 <jwynkoop@hotmail.com>
2026-05-03 21:44:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ##st.dataframe(data=my_dataframe, use_container_width=True)
 
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-##st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -49,6 +46,11 @@ if ingredients_list:
 
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
+
+          
+            pd_df=my_dataframe.to_pandas()
+            st.dataframe(pd_df)
+            ##st.stop()
 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
